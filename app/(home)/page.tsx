d2d65a1d9d9bacaf3fd006ab50eab2200fc4252a
Toni Rachev <tonirachev92@gmail.com>
2024-12-29 17:35:14 +0200

    Update page.tsx

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,33 @@
 import Hero from "@/app/(home)/components/hero";
+import {fetchLatestProducts} from "@/lib/api/product.api";
+import FeaturedProducts from "@/components/featured-products";
 
-export default function Home() {
+const featuredProductSections = {
+    newArrivals: {
+        title: 'New Arrivals',
+        caption: 'New Styles, New You: Explore what’s just arrived',
+    },
+    featured: {
+        title: 'Featured',
+        caption: 'Handpicked Just for You: Discover Our Top Picks of the Season',
+    }
+}
+
+export default async function Home() {
+    const products = await fetchLatestProducts();
+
     return (
         <>
             <Hero/>
+            <FeaturedProducts
+                products={products}
+                meta={featuredProductSections.newArrivals}
+            />
+
+            <FeaturedProducts
+                products={products}
+                meta={featuredProductSections.featured}
+            />
         </>
     )
 }
