5ad590e0ea2cbf29fdb3d6d1e88aa8eaff27dde3
Toni Rachev <tonirachev92@gmail.com>
2024-12-29 11:27:54 +0200

    add wrapper

--- components/navigation/navigation.tsx
+++ components/navigation/navigation.tsx
@@ -1,5 +1,22 @@
 import Logo from "@/components/navigation/ui/logo";
 import MainMenu from "@/components/navigation/ui/main-menu";
+import AccountMenu from "@/components/navigation/ui/account-menu";
+
+type WrapperProps = {
+    basis: string;
+    justify?: 'start' | 'center' | 'end';
+    children: React.ReactNode;
+}
+
+const Wrapper = ({basis, justify, children}: WrapperProps) => {
+    const wrapperClasses = `flex basis-[${basis}%] ${justify ? `justify-${justify}` : ''}`;
+
+    return (
+        <div className={wrapperClasses}>
+            {children}
+        </div>
+    )
+}
 
 export default function Navigation() {
     return (
