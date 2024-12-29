a68401e02ddb93d6f79812e021e7cb13f6f25810
Toni Rachev <tonirachev92@gmail.com>
2024-12-29 18:05:30 +0200

    add fetchLatestCollections

--- lib/api/product.api.ts
+++ lib/api/product.api.ts
@@ -1,6 +1,7 @@
 import {client} from "@/lib/shopify";
+import {CollectionMeta, FeaturedProduct} from "@/lib/definitions";
 
-export const fetchLatestProducts = async () => {
+export const fetchLatestProducts = async (): Promise<FeaturedProduct[]> => {
     const query = `
  query MyQuery {
   products(first: 4, sortKey: CREATED_AT, reverse: true) {
@@ -28,4 +29,23 @@ export const fetchLatestProducts = async () => {
     const {data} = await client.request(query);
 
     return data.products.nodes;
+}
+
+export const fetchLatestCollections = async (): Promise<CollectionMeta[]> => {
+    const query = `
+    query MyQuery {
+  collections(first: 3) {
+    nodes {
+      title
+      id
+      image {
+        url
+      }
+    }
+  }
+}`;
+
+    const {data} = await client.request(query);
+
+    return data.collections.nodes;
 }
