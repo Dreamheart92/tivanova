da470f5e356c93af142c4b0def4a63d208c9f21f
Toni Rachev <tonirachev92@gmail.com>
2024-12-29 18:04:50 +0200

    Create featured-collections.tsx

---
+++ app/(home)/components/featured-collections.tsx
@@ -0,0 +1,19 @@
+import {CollectionMeta} from "@/lib/definitions";
+import CollectionCard from "@/components/colleciton-card";
+import ContentContainer from "@/components/ui/content-container";
+
+type FeaturedCollectionsProps = {
+    collections: CollectionMeta[];
+}
+
+export default function FeaturedCollections({collections}: FeaturedCollectionsProps) {
+    return (
+        <ContentContainer>
+            <div className='flex gap-4'>
+                {collections.map((collection) => (
+                    <CollectionCard key={collection.id} collection={collection}/>
+                ))}
+            </div>
+        </ContentContainer>
+    )
+}
