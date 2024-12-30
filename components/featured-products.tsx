c8b602f3bef807dd016f258b608d62971a7b3b33
Toni Rachev <tonirachev92@gmail.com>
2024-12-30 13:11:17 +0200

    add mobile wrap

--- components/featured-products.tsx
+++ components/featured-products.tsx
@@ -14,7 +14,7 @@ type FeaturedProductsProps = {
 
 const SectionMetaData = ({meta}: { meta: Meta }) => {
     return (
-        <div className='pb-4'>
+        <div className='pb-4 text-[1.4em] lg:text-[1em] text-center lg:text-left'>
             <h3>{meta.title}</h3>
             <h4>{meta.caption}</h4>
         </div>
@@ -27,7 +27,7 @@ export default function FeaturedProducts({products, meta}: FeaturedProductsProps
             <div className=''>
                 <SectionMetaData meta={meta}/>
 
-                <div className='flex gap-4'>
+                <div className='flex gap-4 flex-wrap xl:flex-nowrap'>
                     {products.map((product) => (
                         <ProductCard key={product.id} product={product}/>
                     ))}
