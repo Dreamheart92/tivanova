d36d1f6f89daf1abc11991768d289fa668043f2a
Toni Rachev <tonirachev92@gmail.com>
2024-12-30 13:09:48 +0200

    hide on mobile

--- app/(home)/components/featured-collection.tsx
+++ app/(home)/components/featured-collection.tsx
@@ -4,7 +4,9 @@ import Image from "next/image";
 export default function FeaturedCollection() {
     return (
         <ContentContainer>
-            <Image src='/featured-collection.jpeg' alt='Featured Collection' width={1700} height={500}/>
+            <div className='hidden lg:block'>
+                <Image src='/featured-collection.jpeg' alt='Featured Collection' width={1700} height={500}/>
+            </div>
         </ContentContainer>
     )
 }
