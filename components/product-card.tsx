1c94d8742030ac7400d4c6fa9ab86cd636c03895
Toni Rachev <tonirachev92@gmail.com>
2024-12-31 17:00:24 +0200

    Update product-card.tsx

--- components/product-card.tsx
+++ components/product-card.tsx
@@ -3,6 +3,9 @@
 import {FeaturedProduct} from "@/lib/definitions";
 import Image from "next/image";
 import {useState} from "react";
+import Link from "next/link";
+import {PATHS} from "@/lib/constants/paths";
+import {getIdFromShopifyString} from "@/lib/utils";
 
 type ProductCardProps = {
     product: FeaturedProduct;
@@ -12,44 +15,46 @@ export default function ProductCard({product}: ProductCardProps) {
     const [hovered, setIsHovered] = useState(false);
 
     return (
-        <div className="relative text-[1.6em] md:text-[1.2em] lg:text-[1em] cursor-pointer basis-[100%] sm:basis-[calc(100%/2-1em)] md:basis-[calc(100%/3-1em)]">
-            <div
-                className="relative"
-                onMouseEnter={() => setIsHovered(true)}
-                onMouseLeave={() => setIsHovered(false)}
-            >
-                <Image
-                    src={product.images.edges[0].node.url}
-                    alt={product.title}
-                    layout="responsive"
-                    width={100}
-                    height={110}
-                    className="max-h-[45rem] sm:max-h-[28rem] md:max-h-[22rem] lg:max-h-[28rem] xl:max-h-[27rem] 2xl:max-h-[35rem]"
-                    style={{
-                        opacity: hovered ? 0 : 1,
-                        transition: 'opacity 0.3s ease-in',
-                        objectFit: 'cover',
-                        objectPosition: 'center',
-                    }}
-                />
+        <div
+            className="relative text-[1.6em] md:text-[1.2em] lg:text-[1em] cursor-pointer basis-[100%] sm:basis-[calc(100%/2-1em)] md:basis-[calc(100%/3-1em)]">
+            <Link href={PATHS.PRODUCT_DETAILS(getIdFromShopifyString(product.id))}>
+                <div
+                    className="relative"
+                    onMouseEnter={() => setIsHovered(true)}
+                    onMouseLeave={() => setIsHovered(false)}
+                >
+                    <Image
+                        src={product.images.edges[0].node.url}
+                        alt={product.title}
+                        width={500}
+                        height={110}
+                        className="max-h-[45rem] sm:max-h-[28rem] md:max-h-[22rem] lg:max-h-[28rem] xl:max-h-[27rem] 2xl:max-h-[35rem]"
+                        style={{
+                            opacity: hovered ? 0 : 1,
+                            transition: 'opacity 0.3s ease-in',
+                            objectFit: 'cover',
+                            objectPosition: 'center',
+                        }}
+                    />
 
-                <Image
-                    src={product.images.edges[1].node.url}
-                    alt={product.title}
-                    fill
-                    style={{
-                        objectFit: 'cover',
-                        transition: 'opacity 0.3s ease-in',
-                        opacity: hovered ? 1 : 0,
-                    }}
-                />
-            </div>
+                    <Image
+                        src={product.images.edges[1].node.url}
+                        alt={product.title}
+                        fill
+                        style={{
+                            objectFit: 'cover',
+                            transition: 'opacity 0.3s ease-in',
+                            opacity: hovered ? 1 : 0,
+                        }}
+                    />
+                </div>
 
-            <div className='py-2 px-1 lg:p-0'>
-                <h4 className='uppercase font-bold'>{product.vendor}</h4>
-                <h5>{product.title}</h5>
-                <p className='font-light'>{Number(product.priceRange.minVariantPrice.amount).toFixed(2)} EUR</p>
-            </div>
+                <div className='py-2 px-1 lg:p-0'>
+                    <h4 className='uppercase font-bold'>{product.vendor}</h4>
+                    <h5>{product.title}</h5>
+                    <p className='font-light'>{Number(product.priceRange.minVariantPrice.amount).toFixed(2)} EUR</p>
+                </div>
+            </Link>
         </div>
     )
 }
