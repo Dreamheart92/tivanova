142057993b65c807f4a2af02c323e943ec0946e6
Toni Rachev <tonirachev92@gmail.com>
2024-12-31 16:55:47 +0200

    add organizeProductVariants util

---
+++ lib/product.utils.ts
@@ -0,0 +1,31 @@
+import {ProductVariant, Variant} from "@/lib/definitions";
+
+export const organizeProductVariants = (variants: ProductVariant[]): Variant[] => {
+    const productVariants: Variant[] = [];
+
+    variants.forEach((edge => {
+        const [color, size] = edge.node.selectedOptions;
+
+        const colorIndex = productVariants.findIndex((variation) => variation.color === color.value);
+
+        if (colorIndex === -1) {
+            productVariants.push({
+                color: color.value,
+                price: edge.node.price,
+                id: edge.node.id,
+                options: [{
+                    name: size.value,
+                    id: edge.node.id,
+                }],
+            })
+        } else {
+            productVariants[colorIndex].options.push({
+                name: size.value,
+                id: edge.node.id,
+            });
+        }
+    }))
+
+    return productVariants;
+}
+
