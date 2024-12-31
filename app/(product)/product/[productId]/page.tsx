ccce4ea2a27eaeaa39fb632a501f55ca1571a1c2
Toni Rachev <tonirachev92@gmail.com>
2024-12-31 17:07:31 +0200

    add async product page with suspense and product wrapper

--- app/(product)/product/[productId]/page.tsx
+++ app/(product)/product/[productId]/page.tsx
@@ -1,3 +1,15 @@
+import {fetchProductById} from "@/lib/api/product.api";
+import {organizeProductVariants} from "@/lib/product.utils";
+import {notFound} from "next/navigation";
+import {ProductSkeleton} from "@/components/skeletons";
+import ProductGallery from "@/app/(product)/product/[productId]/components/product-gallery";
+import ProductDetails from "@/app/(product)/product/[productId]/components/product-details";
+import {Suspense} from "react";
+
+type ProductProps = {
+    params: Promise<{ productId: string }>;
+}
+
 const ProductWrapper = async ({productId}: { productId: string }) => {
     const product = await fetchProductById(productId);
 
@@ -12,5 +24,15 @@ const ProductWrapper = async ({productId}: { productId: string }) => {
             <ProductGallery images={product.images.edges}/>
             <ProductDetails product={product} variants={productVariants}/>
         </div>
+    )
+}
+
+export default async function Product({params}: ProductProps) {
+    const {productId} = await params;
+
+    return (
+        <Suspense fallback={<ProductSkeleton/>}>
+            <ProductWrapper productId={productId}/>
+        </Suspense>
     )
 }
