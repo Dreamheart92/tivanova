543469c32bba91d869a5550f43adf21fb3855e1e
Toni Rachev <tonirachev92@gmail.com>
2024-12-31 16:58:57 +0200

    created ProductCardSekelton

---
+++ components/skeletons.tsx
@@ -0,0 +1,15 @@
+import ContentContainer from "@/components/ui/content-container";
+
+import {Skeleton} from "@/components/ui/skeleton";
+
+export const ProductCardSkeleton = () => {
+    return (
+        <div className='basis-[100%] sm:basis-[calc(100%/2-1em)] md:basis-[calc(100%/4-1em)]'>
+            <Skeleton
+                className='h-[45rem] sm:h-[28rem] md:h-[22rem] lg:h-[28rem] xl:h-[27rem] 2xl:h-[35rem] rounded-none'/>
+            <Skeleton className='w-[3em] h-[1em] rounded-none mt-2'/>
+            <Skeleton className='w-[8em] h-[1em] rounded-none mt-2'/>
+            <Skeleton className='w-[3em] h-[1em] rounded-none mt-2'/>
+        </div>
+    )
+}
