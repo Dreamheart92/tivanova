e2caa97e6cf1675959df2bc101e399da0cbc4737
Toni Rachev <tonirachev92@gmail.com>
2024-12-31 16:54:16 +0200

    add getIdFromShopifyString util

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const getIdFromShopifyString = (shopifyString: string) => {
+  const chunks = shopifyString.split("/");
+  return chunks[chunks.length - 1];
+}
+
