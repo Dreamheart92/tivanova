01d5890196f25c92a4abee90d8e8e729bbad7c8f
Toni Rachev <tonirachev92@gmail.com>
2025-01-02 12:56:33 +0200

    Create announcement-banner.tsx

---
+++ app/(product)/catalogue/[[...catalogueParams]]/components/announcement-banner.tsx
@@ -0,0 +1,7 @@
+export default function AnnouncementBanner() {
+    return (
+        <div className='text-center bg-red-500 py-3 mb-12 uppercase text-white'>
+            <h5 className='animate-bounce font-bold'>New Collection Just Dropped</h5>
+        </div>
+    );
+}
