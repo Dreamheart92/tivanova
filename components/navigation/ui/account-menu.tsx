23caf44d2daabaa87fba2ef4ea5cdfd14d5861d7
Toni Rachev <tonirachev92@gmail.com>
2025-01-02 12:55:21 +0200

    added paths to the account menu

--- components/navigation/ui/account-menu.tsx
+++ components/navigation/ui/account-menu.tsx
@@ -1,16 +1,21 @@
 import {NavigationMenu, NavigationMenuItem} from "@/components/ui/navigation-menu";
 import {Heart, Search, ShoppingBag, UserRound} from "lucide-react";
+import Link from "next/link";
+import {PATHS} from "@/lib/constants/paths";
 
 const ICON_WIDTH = 16;
 
 type IconWrapperProps = {
-    Icon: React.ComponentType<React.SVGProps<SVGSVGElement>>
+    Icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+    href: string;
 }
 
-const IconWrapper = ({Icon}: IconWrapperProps) => {
+const IconWrapper = ({Icon, href}: IconWrapperProps) => {
     return (
         <NavigationMenuItem>
-            <Icon width={ICON_WIDTH} cursor='pointer'/>
+            <Link href={href}>
+                <Icon width={ICON_WIDTH} cursor='pointer'/>
+            </Link>
         </NavigationMenuItem>
     )
 }
@@ -18,10 +23,10 @@ const IconWrapper = ({Icon}: IconWrapperProps) => {
 export default function AccountMenu() {
     return (
         <NavigationMenu className="gap-3">
-            <IconWrapper Icon={Heart}/>
-            <IconWrapper Icon={UserRound}/>
-            <IconWrapper Icon={Search}/>
-            <IconWrapper Icon={ShoppingBag}/>
+            <IconWrapper Icon={Heart} href={PATHS.LOGIN}/>
+            <IconWrapper Icon={UserRound} href={PATHS.LOGIN}/>
+            <IconWrapper Icon={Search} href={PATHS.LOGIN}/>
+            <IconWrapper Icon={ShoppingBag} href={PATHS.LOGIN}/>
         </NavigationMenu>
     )
 }
