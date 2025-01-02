224c6ca7f0e87220086131f905d94ca7eab0284c
Toni Rachev <tonirachev92@gmail.com>
2025-01-02 13:19:09 +0200

    Update page.tsx

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -57,6 +57,7 @@ export default async function Home() {
     return (
         <div className='flex flex-col gap-4 lg:gap-12'>
             <Hero title={pageSettings.hero.title}/>
+
             <Suspense fallback={<FeaturedProductsSkeleton/>}>
                 <NewArrivalsWrapper/>
             </Suspense>
