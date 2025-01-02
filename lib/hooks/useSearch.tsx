6d06414df1af0c32ac4c53ba06545506a568a1d4
Toni Rachev <tonirachev92@gmail.com>
2025-01-02 12:53:08 +0200

    add useSearch hook

---
+++ lib/hooks/useSearch.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import {usePathname, useRouter, useSearchParams} from "next/navigation";
+import {useCallback} from "react";
+
+const useQueryParams = () => {
+    const pathname = usePathname();
+    const searchParams = useSearchParams();
+    const router = useRouter();
+
+    const updateQueryParam = useCallback((key: string, value: string) => {
+        const params = new URLSearchParams(searchParams);
+
+        const searchParamValues = params.get(key)?.split('|') || [];
+
+        const searchParamValueIndex = searchParamValues.findIndex((paramValue) => paramValue === value);
+
+        if (searchParamValueIndex === -1) {
+            const updatedParamValues = [...searchParamValues, value];
+            params.set(key, updatedParamValues.join('|'));
+        } else {
+            const updatedParamValues = searchParamValues.filter((paramValue) => paramValue !== value);
+
+            if (updatedParamValues.length > 0) {
+                params.set(key, updatedParamValues.join('|'));
+            } else {
+                params.delete(key);
+            }
+        }
+
+        router.replace(`${pathname}?${params.toString()}`);
+
+    }, [searchParams])
+
+    return {
+        updateQueryParam,
+    }
+}
+
+export default useQueryParams;
