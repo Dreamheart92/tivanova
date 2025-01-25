81868506c26cb4067dd13d4cadf413e11cc9e48c
Toni Rachev <tonirachev92@gmail.com>
2025-01-25 11:26:29 +0200

    Create cart.ts

---
+++ lib/utils/server/cart.ts
@@ -0,0 +1,17 @@
+'use server';
+
+import {CartType} from "@/lib/definitions/cart";
+import {addToCart, fetchCart} from "@/lib/api/cart.api";
+import {revalidateTag} from "next/cache";
+import {TAGS} from "@/lib/constants/tags";
+import {convertProductToShopifyCartLines, getUniqueGuestCartItems} from "@/lib/utils/cart";
+
+export const transferGuestCartToCustomerCart = async (customerCartId: string, guestCart: CartType) => {
+    const customerCart = await fetchCart(customerCartId);
+    const guestCartProducts = getUniqueGuestCartItems(guestCart, customerCart as CartType);
+
+    const lines = convertProductToShopifyCartLines(guestCartProducts);
+
+    await addToCart(lines, customerCartId);
+    revalidateTag(TAGS.CART);
+}
