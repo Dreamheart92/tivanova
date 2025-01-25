66773bd4315665adc4ee835989b3e30e29ee2c1d
Toni Rachev <tonirachev92@gmail.com>
2025-01-25 18:58:15 +0200

    add protected and public routes

--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,19 @@ import {NextResponse} from "next/server";
 import type {NextRequest} from "next/server";
 import {getCountryIsoBasedOnIp} from "@/lib/utils/server/customer.server-utils";
 import {SETTINGS} from "@/settings";
+import {getSession} from "@/lib/utils/server/session.utils";
+import {PATHS} from "@/lib/constants/paths";
+
+const publicRoutes = [PATHS.LOGIN, PATHS.SIGNUP];
+const protectedRoutes = [PATHS.ACCOUNT];
 
 export async function middleware(req: NextRequest) {
     const response = NextResponse.next();
+    const session = await getSession();
+
+    const path = req.nextUrl.pathname;
+    const isPublicRoute = publicRoutes.includes(path);
+    const isProtectedRoute = protectedRoutes.some((route) => route === path || path.startsWith(route));
 
     if (!req.cookies.get('countryISO')) {
         const customerIp = req.headers.get('x-forwarded-for') || '127.0.0.1';
@@ -18,11 +28,17 @@ export async function middleware(req: NextRequest) {
         }
     }
 
+    if (isProtectedRoute && !session) {
+        return NextResponse.redirect(new URL(PATHS.LOGIN, req.nextUrl));
+    }
+
+    if (isPublicRoute && session) {
+        return NextResponse.redirect(new URL(PATHS.ACCOUNT, req.nextUrl));
+    }
+
     return response;
 }
 
 export const config = {
-    matcher: [
-        '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
-    ],
+    matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 }
