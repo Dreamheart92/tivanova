6ff8e9b6d4610f2b92b7c794f6f62cd8ef6f3ad1
Toni Rachev <tonirachev92@gmail.com>
2025-01-25 11:25:50 +0200

    Update shopify.ts

--- lib/api/shopify.ts
+++ lib/api/shopify.ts
@@ -1,4 +1,21 @@
-import {adminClient, client} from "@/lib/shopify";
+import {createStorefrontApiClient} from "@shopify/storefront-api-client";
+import {createAdminApiClient} from "@shopify/admin-api-client";
+
+const domain = process.env.SHOPIFY_DOMAIN;
+const token = process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN;
+const adminApiAccessToken = process.env.SHOPIFY_ADMIN_API_ACCESS_TOKEN;
+
+export const client = createStorefrontApiClient({
+    storeDomain: domain as string,
+    apiVersion: '2025-01',
+    privateAccessToken: token,
+})
+
+export const adminClient = createAdminApiClient({
+    storeDomain: domain as string,
+    apiVersion: '2025-01',
+    accessToken: adminApiAccessToken as string,
+})
 
 export const clientFetcher = async (query: string, variables?: any) => {
     const {data, errors} = await client.request(query, {
