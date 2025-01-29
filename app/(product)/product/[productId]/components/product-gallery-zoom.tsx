ffae14e5dbcda969bbc0ab76969b2257e0fe59cb
Toni Rachev <tonirachev92@gmail.com>
2025-01-29 11:48:43 +0200

    add mobile style

--- app/(product)/product/[productId]/components/product-gallery-zoom.tsx
+++ app/(product)/product/[productId]/components/product-gallery-zoom.tsx
@@ -28,17 +28,24 @@ export default function ProductGalleryZoom({images, open, onClose, selectedImage
                 <VisuallyHidden>Product Image Gallery</VisuallyHidden>
             </DialogTitle>
             <DialogContent
-                className='min-w-full h-full p-0 m-0 border-0'
+                className='min-w-full h-full p-0 m-0 border-0 overflow-y-scroll'
                 style={{borderRadius: 'unset'}}
             >
-                <div className='w-full h-full overflow-y-scroll'>
+                <div className='w-full h-full'>
                     {selectedImage !== null && (
-                        <Image src={images[selectedImage].url} alt='Product Image' width={1920} height={1000}
-                               quality={100}/>
+                        <Image
+                            src={images[selectedImage].url}
+                            alt='Product Image'
+                            width={1920}
+                            height={1000}
+                            quality={100}
+                            className='w-full min-h-screen object-cover object-center'
+                        />
                     )}
                 </div>
 
-                <div className='absolute left-8 top-1/2 animate-slideInFromLeft'>
+                <div
+                    className='absolute bottom-0 flex justify-center md:block gap-4 w-full md:left-8 md:top-1/2 animate-slideInFromBottom md:animate-slideInFromLeft'>
                     {images.map((image, index) => (
                         <Image
                             key={image.url}
