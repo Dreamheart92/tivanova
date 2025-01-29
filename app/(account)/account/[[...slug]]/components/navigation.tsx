83853c7e80f3815aae4b4ce187a3cb53c4d6ee3f
Toni Rachev <tonirachev92@gmail.com>
2025-01-29 11:33:26 +0200

    add mobile prop

--- app/(account)/account/[[...slug]]/components/navigation.tsx
+++ app/(account)/account/[[...slug]]/components/navigation.tsx
@@ -31,7 +31,7 @@ const MenuItem = ({label, href}: MenuItemProps) => {
             <Link href={href} passHref legacyBehavior>
                 <NavigationMenuLink>
                     <div
-                        className={clsx('flex justify-between text-stone-500 items-center w-72 p-3 hover:text-stone-900', {
+                        className={clsx('flex justify-between text-stone-500 items-center w-screen lg:w-72 p-6 lg:p-3 hover:text-stone-900', {
                             '!text-stone-900 font-bold': selected,
                         })}>
                         <p>{label}</p>
@@ -43,21 +43,21 @@ const MenuItem = ({label, href}: MenuItemProps) => {
     )
 }
 
-export default function Navigation() {
+export default function Navigation({mobile}: { mobile?: boolean }) {
     return (
-        <NavigationMenu>
-            <NavigationMenuList className='flex-col bg-white rounded-sm'>
-                <MenuItem label='Orders' href={PATHS.ACCOUNT}/>
-                {/*<Separator/>*/}
+        <NavigationMenu className={clsx('hidden lg:block', {
+            'block lg:hidden': mobile
+        })}>
+            <NavigationMenuList className='flex-col bg-white rounded-sm text-[3em] lg:text-base'>
+                <MenuItem label='My Account' href={PATHS.ACCOUNT}/>
+                <MenuItem label='Orders' href={PATHS.ORDERS}/>
                 <MenuItem label='Wishlist' href={PATHS.WISHLIST}/>
-                {/*<Separator/>*/}
                 <MenuItem label='Settings' href={PATHS.ACCOUNT_NAVIGATION.SETTINGS}/>
-                {/*<Separator/>*/}
                 <NavigationMenuItem className='w-full justify-start !m-0'>
                     <form>
                         <Button
                             variant='link'
-                            className='w-full justify-start pl-3 my-1 text-[1em] font-normal !no-underline text-stone-500 hover:text-stone-900 transition-none'
+                            className='w-full justify-start p-6 lg:p-3 text-[1em] font-normal !no-underline text-stone-500 hover:text-stone-900 transition-none'
                             formAction={logout}
                         >
                             Logout
