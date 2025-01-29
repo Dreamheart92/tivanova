2540e69f7d2d176821c217e46e70af1e42fae40e
Toni Rachev <tonirachev92@gmail.com>
2025-01-29 11:49:42 +0200

    add mobile style

--- components/navigation/mobile-navigation.tsx
+++ components/navigation/mobile-navigation.tsx
@@ -4,7 +4,7 @@ import AccountMenu from "@/components/navigation/ui/account-menu";
 
 export default function MobileNavigation({isAuth}: { isAuth: boolean }) {
     return (
-        <div className='flex lg:hidden justify-between items-center py-2 px-1'>
+        <div className='flex lg:hidden justify-between items-center py-4 px-2 lg:py-2 lg:px-1 text-sm lg:text-base'>
             <div className='flex gap-2 items-center'>
                 <AlignJustify/>
                 <Logo/>
