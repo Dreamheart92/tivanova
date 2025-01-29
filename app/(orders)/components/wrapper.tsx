40ffa0167e8262c5a8f312285ee1deec9f015f42
Toni Rachev <tonirachev92@gmail.com>
2025-01-29 11:47:39 +0200

    add mobile padding

--- app/(orders)/components/wrapper.tsx
+++ app/(orders)/components/wrapper.tsx
@@ -1,6 +1,6 @@
 export default function Wrapper({children}: { children: React.ReactNode }) {
     return (
-        <div className='bg-white p-4 shadow-sm'>
+        <div className='bg-white py-4 md:p-4 shadow-sm'>
             {children}
         </div>
     )
