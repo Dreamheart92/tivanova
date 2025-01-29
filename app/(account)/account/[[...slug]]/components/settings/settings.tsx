a0167a1dcb465b627612f047d04ebb981fbdd3c2
Toni Rachev <tonirachev92@gmail.com>
2025-01-29 11:33:57 +0200

    add mobile text size

--- app/(account)/account/[[...slug]]/components/settings/settings.tsx
+++ app/(account)/account/[[...slug]]/components/settings/settings.tsx
@@ -2,14 +2,19 @@ import {CustomerType} from "@/lib/definitions/customer.definitions";
 import ChangePassword from "@/app/(account)/account/[[...slug]]/components/settings/change-password";
 import DeliveryAddress from "@/app/(account)/account/[[...slug]]/components/settings/delivery-address";
 import MyDetails from "@/app/(account)/account/[[...slug]]/components/settings/my-details";
+import {Separator} from "@/components/ui/separator";
 
 export default async function Settings({customer}: { customer: CustomerType }) {
 
     return (
-        <div className='flex flex-col gap-4 w-full'>
-            <MyDetails customer={customer}/>
-            <DeliveryAddress/>
-            <ChangePassword/>
+        <div className='w-full p-6 lg:p-0 text-[2em] lg:text-base'>
+            <h2>My Settings</h2>
+            <Separator className='my-4'/>
+            <div className='flex flex-col gap-4 w-full'>
+                <MyDetails customer={customer}/>
+                <DeliveryAddress/>
+                <ChangePassword/>
+            </div>
         </div>
     )
 }
