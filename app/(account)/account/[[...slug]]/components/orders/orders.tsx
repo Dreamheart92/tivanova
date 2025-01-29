337ae874b0fd9d8d9c8d58d9e812e445f058c7f5
Toni Rachev <tonirachev92@gmail.com>
2025-01-29 11:33:37 +0200

    add explore shop button

--- app/(account)/account/[[...slug]]/components/orders/orders.tsx
+++ app/(account)/account/[[...slug]]/components/orders/orders.tsx
@@ -1,6 +1,9 @@
 import {Separator} from "@/components/ui/separator";
 import {OrderSummaryType} from "@/lib/definitions/order.definitions";
 import OrderCard from "@/app/(account)/account/[[...slug]]/components/orders/order-card";
+import {Button} from "@/components/ui/button";
+import Link from "next/link";
+import {PATHS} from "@/lib/constants/paths";
 
 type OrdersProps = {
     orders: OrderSummaryType[];
@@ -8,16 +11,27 @@ type OrdersProps = {
 
 export default function Orders({orders}: OrdersProps) {
     return (
-        <div className='basis-3/4'>
-            <h2>My orders</h2>
-            <Separator className='my-2'/>
+        <div className='w-full lg:basis-3/4 p-6 lg:p-0 text-[2em] lg:text-base'>
+            <h2>My Orders</h2>
+            <Separator className='my-4'/>
 
             {orders.length > 0 ? (
                     orders.map((order) => (
                         <OrderCard key={order.id} order={order}/>
                     ))
                 )
-                : <p>You still do not have any purchases</p>
+                : (
+                    <>
+                        <p>You still do not have any purchases</p>
+                        <div className='w-full mt-2'>
+                            <Link href={PATHS.SHOP}>
+                                <Button>
+                                    Explore shop
+                                </Button>
+                            </Link>
+                        </div>
+                    </>
+                )
             }
         </div>
     )
