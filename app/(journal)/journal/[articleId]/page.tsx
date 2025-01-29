15282d2c29efc71beee6c45594b8eefc6c59eeab
Toni Rachev <tonirachev92@gmail.com>
2025-01-29 11:46:01 +0200

    add mobile style

--- app/(journal)/journal/[articleId]/page.tsx
+++ app/(journal)/journal/[articleId]/page.tsx
@@ -13,15 +13,18 @@ const JournalWrapper = async ({articleId}: { articleId: string }) => {
     }
 
     return (
-        <div className='max-w-screen-xl mx-auto'>
-            <div className='relative mb-28'>
-                <Image src={article.image.url} alt={article.title} width={1280} height={500} className='max-h-[750px] object-cover'/>
-                <div className='absolute -bottom-20'>
-                    <div className='bg-stone-100 max-w-[70%] p-8 pl-0'>
-                        <p className='text-stone-500'>{moment(article.publishedAt).format('MMMM DD, YYYY')}</p>
+        <div className='max-w-screen-xl mx-auto text-sm md:text-base p-2 md:p-0'>
+            <div className='relative mb-4 md:mb-28'>
+                <Image src={article.image.url} alt={article.title} width={1280} height={500}
+                       className='max-h-[750px] object-cover'/>
+
+                <div className='md:absolute md:-bottom-20'>
+                    <div className='bg-stone-100 md:max-w-[70%] md:p-8 pl-0'>
+                        <p className='pt-2 md:pt-0 text-stone-500'>{moment(article.publishedAt).format('MMMM DD, YYYY')}</p>
                         <h1>{article.title}</h1>
                     </div>
                 </div>
+
             </div>
 
             <div className='text-[1.1em] leading-relaxed' dangerouslySetInnerHTML={{__html: article.contentHtml}}/>
