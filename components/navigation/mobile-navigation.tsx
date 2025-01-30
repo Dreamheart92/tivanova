5603b84d8c39d81f59df91a2ed56dd265751ff3e
Toni Rachev <tonirachev92@gmail.com>
2025-01-30 14:24:03 +0200

    add mobile menu

--- components/navigation/mobile-navigation.tsx
+++ components/navigation/mobile-navigation.tsx
@@ -1,12 +1,12 @@
-import Logo from "@/components/navigation/ui/logo";
-import {AlignJustify} from "lucide-react";
-import AccountMenu from "@/components/navigation/ui/account-menu";
+import Logo from "@/components/navigation/components/logo";
+import AccountMenu from "@/components/navigation/components/account-menu";
+import MobileMenu from "@/components/navigation/components/mobile-menu";
 
 export default function MobileNavigation({isAuth}: { isAuth: boolean }) {
     return (
         <div className='flex lg:hidden justify-between items-center py-4 px-2 lg:py-2 lg:px-1 text-sm lg:text-base'>
             <div className='flex gap-2 items-center'>
-                <AlignJustify/>
+                <MobileMenu/>
                 <Logo/>
             </div>
 
@@ -14,3 +14,4 @@ export default function MobileNavigation({isAuth}: { isAuth: boolean }) {
         </div>
     )
 }
+
